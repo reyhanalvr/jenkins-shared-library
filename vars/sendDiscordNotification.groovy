58f605a34a1c5fe7bbf6785f206310265be51a33
reyhanalvr <91837320+reyhanalvr@users.noreply.github.com>
2024-09-29 02:35:25 +0700

    Update sendDiscordNotification.groovy

--- vars/sendDiscordNotification.groovy
+++ vars/sendDiscordNotification.groovy
@@ -1,13 +1,23 @@
 def call(String title, String message, String status, String webhookUrl) {
-    // Buat payload untuk notifikasi Discord
+
+    def color = status == "success" ? "3066993" : "15158332" 
     def payload = [
-        content: "${title}\n${message}\nStatus: ${status}"
+        embeds: [
+            [
+                title: title,
+                description: message,
+                color: color,
+                footer: [
+                    text: "Status: ${status}",
+                ],
+                timestamp: new Date().format("yyyy-MM-dd'T'HH:mm:ss'Z", TimeZone.getTimeZone('UTC'))
+            ]
+        ]
     ]
 
     // Kirim notifikasi ke Discord menggunakan curl
     def response = sh(script: "curl -H 'Content-Type: application/json' -d '${groovy.json.JsonOutput.toJson(payload)}' ${webhookUrl}", returnStatus: true)
 
-    // Cek status pengiriman
     if (response != 0) {
         error "Failed to send Discord notification"
     }
