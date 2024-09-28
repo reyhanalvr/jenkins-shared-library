1514bf7ebcff3f0305f83c671fb5c9cd3a45cf77
reyhanalvr <91837320+reyhanalvr@users.noreply.github.com>
2024-09-29 01:50:54 +0700

    Create discordNotifier.groovy

---
+++ vars/discordNotifier.groovy
@@ -0,0 +1,22 @@
+import groovy.json.JsonOutput
+
+def sendNotification(String title, String description, String status, String webhookUrl) {
+    def color = (status == "success") ? 65280 : 16711680 // Hijau untuk sukses, Merah untuk error
+    def payload = [
+        embeds: [
+            [
+                title: title,
+                description: description,
+                color: color,
+                footer: [
+                    text: "Notifikasi dari Jenkins"
+                ]
+            ]
+        ]
+    ]
+    
+    def jsonPayload = JsonOutput.toJson(payload)
+    sh """
+    curl -H "Content-Type: application/json" -d '${jsonPayload}' ${webhookUrl}
+    """
+}
